4575dae1f9a58359f8d5ad048efa4ba57e572e7f
孤城回望苍烟合 <flynn.he@udpac.com>
2020-03-09 20:07:45 +0800

    Update check_if_epic_link_exist.java

--- jira/check_if_epic_link_exist.java
+++ jira/check_if_epic_link_exist.java
@@ -2,7 +2,7 @@ import com.atlassian.jira.component.ComponentAccessor
 import com.opensymphony.workflow.InvalidInputException
 
 def customFieldManager = ComponentAccessor.getCustomFieldManager()
-def epiclinkfield = customFieldManager.getCustomFieldObjectsByName("史诗链接").first()
+def epiclinkfield = customFieldManager.getCustomFieldObjectsByName("史诗链接").first() //Change史诗连接to the actual name of Epic Link
 def epicValue = issue.getCustomFieldValue(epiclinkfield)
 
 log.warn("epicVaue is: " + epicValue)
