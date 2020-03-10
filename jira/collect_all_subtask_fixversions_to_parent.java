517792ab9de101c7981a486d70fdd0ef53e00b94
孤城回望苍烟合 <flynn.he@udpac.com>
2020-03-11 00:47:03 +0800

    Create collect_all_subtask_fixversions_to_parent.java

---
+++ jira/collect_all_subtask_fixversions_to_parent.java
@@ -0,0 +1,33 @@
+import com.atlassian.jira.component.ComponentAccessor
+//import org.apache.log4j.Logger
+//import org.apache.log4j.Level
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.project.Project
+import com.atlassian.jira.project.version.Version
+import com.atlassian.jira.project.version.VersionManager
+import com.atlassian.jira.event.type.EventDispatchOption
+
+def versionManager = ComponentAccessor.getVersionManager()
+def projectManager = ComponentAccessor.getProjectManager()
+def issueManager = ComponentAccessor.getIssueManager()
+def issueLinkManager = ComponentAccessor.getIssueLinkManager()
+//def issue1 = issueManager.getIssueObject("NGP-181")
+
+def parentIssueVersions = issue.getFixVersions()
+//def log = Logger.getLogger("log")
+//log.setLevel(Level.DEBUG)
+
+issueLinkManager.getOutwardLinks(issue.getId()).each {eachLink ->
+    if (eachLink.getIssueLinkType().getName() == "jira_subtask_link") {
+        Issue epicLinkedIssue = eachLink.getDestinationObject()
+        Collection eachIssueVersions = epicLinkedIssue.getFixVersions();
+        for (Version version: eachIssueVersions){
+            //issue.setFixVersions(Arrays.asList(version))
+            parentIssueVersions.add(version)
+            //log.warn(version.getName())
+        }
+    }
+}
+
+issue.setFixVersions(parentIssueVersions)
