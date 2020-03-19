4d04d1a77479cc299cddaae810446b0f96ff6c30
孤城回望苍烟合 <flynn.he@udpac.com>
2020-03-19 23:50:41 +0800

    Update set_assignee_to_project_lead_2.java

--- jira/set_assignee_to_project_lead_2.java
+++ jira/set_assignee_to_project_lead_2.java
@@ -1,11 +1,25 @@
+/**
+    This code run in Scripte Runner in Jia Automation.
+*/
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.project.Project
-import com.atlassian.jira.project.ProjectManager
 import com.atlassian.jira.issue.Issue
-import com.atlassian.jira.issue.MutableIssue
 import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.project.Project
+import com.atlassian.jira.project.version.Version
+import com.atlassian.jira.project.version.VersionManager
+import com.atlassian.jira.event.type.EventDispatchOption
+import com.atlassian.jira.issue.MutableIssue
 
-MutableIssue mutableIssue = ComponentAccessor.getIssueManager().getIssueByCurrentKey(issue.getKey());
+def versionManager = ComponentAccessor.getVersionManager()
+def projectManager = ComponentAccessor.getProjectManager()
+//def project = projectManager.getProjectObjByKey(issue.projectObject.key)
+def issueManager = ComponentAccessor.getIssueManager()
+def issueLinkManager = ComponentAccessor.getIssueLinkManager()
+MutableIssue currentIssue = issueManager.getIssueObject("NGP-228")
+//def issue1 = issueManager.getIssueObject("NGP-228")
+
+//log.setLevel(Level.DEBUG)
 String projectLead = issue.getProjectObject().getProjectLead().getKey()
+currentIssue.setAssigneeId(projectLead)
 
-mutableIssue.setAssigneeId(projectLead)
+issueManager.updateIssue(issue.getProjectObject().getProjectLead(), currentIssue, EventDispatchOption.DO_NOT_DISPATCH, false)
