65567edc1a34d470697480742adce81dd3df8231
孤城回望苍烟合 <flynn.he@udpac.com>
2020-03-19 23:51:32 +0800

    Update set_assignee_to_project-lead.java

--- jira/set_assignee_to_project-lead.java
+++ jira/set_assignee_to_project-lead.java
@@ -1,6 +1,7 @@
 /**
     Set the assignee to project lead.
     Used in script ruuner of post-function.
+    This code can't be used in Script Runner of Jira Automation
   */
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.project.Project
