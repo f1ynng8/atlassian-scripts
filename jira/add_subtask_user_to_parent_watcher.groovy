1fe932ba6e537c7e00c85ad2a75d7f91cf6f6446
孤城回望苍烟合 <flynn.he@udpac.com>
2020-03-27 12:58:27 +0800

    Create add_subtask_user_to_parent_watcher.groovy

---
+++ jira/add_subtask_user_to_parent_watcher.groovy
@@ -0,0 +1,17 @@
+ import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.fields.CustomField
+import com.atlassian.jira.user.ApplicationUser
+
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+def watcherManager = ComponentAccessor.getWatcherManager()
+def userManager = ComponentAccessor.getUserManager()
+
+Issue parentIssue = issue.getParentObject()
+Collection cfs = customFieldManager.getCustomFieldObjectsByName("负责人")
+CustomField cf = cfs[0]
+
+def cfValueObject = issue.getCustomFieldValue(cf) as ApplicationUser
+
+watcherManager.startWatching(cfValueObject, parentIssue)
+
