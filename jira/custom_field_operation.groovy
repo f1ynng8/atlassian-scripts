d5c243aa467fcaa35975879de6e99cb3882c5bbf
孤城回望苍烟合 <flynn.he@udpac.com>
2020-03-28 16:29:56 +0800

    Update custom_field_operation.groovy

--- jira/custom_field_operation.groovy
+++ jira/custom_field_operation.groovy
@@ -1,1 +1,11 @@
-(cfValues['直接移交给开发人员'].getValue() == '是')
+if(cfValues['直接移交给开发人员'].getValue() == "是") 
+    return (cfValues['测试人'] != null)
+else return 1;
+
+if(cfValues['直接移交给开发人员'].getValue() == '是') 
+    return (issue.getComponents().size() != 0)
+else return 1;
+
+if(cfValues['直接移交给开发人员'].getValue() == '是') 
+    return (cfValues['Sprint'] != null)
+else return 1;
