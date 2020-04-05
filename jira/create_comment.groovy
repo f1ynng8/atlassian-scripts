418aad6fca74767fa63b7ceeba21444949a64566
孤城回望苍烟合 <flynn.he@udpac.com>
2020-04-05 23:47:05 +0800

    Update create_comment.groovy

--- jira/create_comment.groovy
+++ jira/create_comment.groovy
@@ -1,3 +1,9 @@
+/*
+* This example script  post function for ScriptRunner for Jira Server shows how you can add a comment to an issue when it is transitioned.
+* "All right, title and interest in this code snippet shall remain the exclusive intellectual property of Adaptavist Group Ltd and its affiliates. Customers with a valid ScriptRunner
+* license shall be granted a  non-exclusive, non-transferable, freely revocable right to use this code snippet only within their own instance of Atlassian products. This licensing notice cannot be removed or
+* amended and must be included in any circumstances where the code snippet is shared by You or a third party." 
+*/ 
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.comments.CommentManager
