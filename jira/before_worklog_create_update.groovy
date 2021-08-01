5819c36f1f1f4b0009082a88a58e4bbab4ebb09a
孤城回望苍烟合 <flynn.he@udpac.com>
2021-08-01 19:18:16 +0800

    Update before_worklog_create_update.groovy

--- jira/before_worklog_create_update.groovy
+++ jira/before_worklog_create_update.groovy
@@ -12,41 +12,8 @@ import com.atlassian.jira.issue.link.IssueLink
 def worklogManager = ComponentAccessor.getWorklogManager()
 def currentUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
 def systemUser = ComponentAccessor.userManager.getUserByName("system")
+def issue = worklog.getIssue()
 
-//检查当天累计工时+当前输入工时
-//---------------------------------------------------------------------------------------------
-def theDate = new Date().format('yyyyMMdd') ;
-def parsedDate = new Date().parse("yyyyMMdd", theDate);
-
-List<Worklog> WorklogList = worklogManager.getWorklogsUpdatedSince(parsedDate.getTime())
-ArrayList<Worklog> processingWorklog = new ArrayList()
-
-for (Worklog worklog in WorklogList){
-  if (worklog.getAuthorObject() == currentUser)
-  processingWorklog.add(worklog)
-}
-
-Long totalSpentTime = 0
-
-for (Worklog worklog in processingWorklog){
-    if(worklog.getStartDate().getTime() > parsedDate.getTime()){
-    	totalSpentTime += worklog.getTimeSpent()
-    }
-}
-
-if ((totalSpentTime + worklogResult.getWorklog().getTimeSpent())/3600 > 16){
-  return "帅哥，你今天的工时已经达到了创纪录的16小时！真的假的？！"
-}
-//---------------------------------------------------------------------------------------------
-return worklogResult.getWorklog().getTimeSpent()+1
-if (worklogResult.getWorklog().getTimeSpent() == 0){
-  return worklogResult.getWorklog().getTimeSpent()+1
-}
-
-//检查输入的时间
-if (worklogResult.getWorklog().getTimeSpent() < 600) { 
-  return "确定只花了不到10分钟处理这件事情吗？如果只是想要推迟处理，请选择“暂停处理”然后选择推迟时间。";
-}
 
 //检查当前状态
 //---------------------------------------------------------------------------------------------
@@ -66,7 +33,6 @@ for (int i = changeHistoryManager.getAllChangeItems(worklog.getIssue()).size()-1
 for (Map.Entry<String, String> entry : tovalue.entrySet()) {
 	    currentStatus = entry.getValue()
 }
-//return currentStatus
 
 if (currentStatus.contains("等待") ||
     currentStatus.contains("完成") ||
@@ -78,13 +44,64 @@ if (currentStatus.contains("等待") ||
 //---------------------------------------------------------------------------------------------
 
 
+//检查当前人员的当天累计工时+当前输入工时
+//---------------------------------------------------------------------------------------------
+def theDate = new Date().format('yyyyMMdd') ;
+def parsedDate = new Date().parse("yyyyMMdd", theDate);
+
+List<Worklog> WorklogList = worklogManager.getWorklogsUpdatedSince(parsedDate.getTime())
+ArrayList<Worklog> processingWorklog = new ArrayList()
+ArrayList<Worklog> currentIssueWorklog = new ArrayList()
+
+for (Worklog worklog in WorklogList){
+  if (worklog.getAuthorObject() == currentUser){
+    processingWorklog.add(worklog)
+    if(worklog.getIssue() == issue){
+      currentIssueWorklog.add(worklog)
+    }
+  }
+}
+
+Long totalSpentTime = 0
+
+for (Worklog worklog in processingWorklog){
+    if(worklog.getStartDate().getTime() > parsedDate.getTime()){
+    	totalSpentTime += worklog.getTimeSpent()
+    }
+}
+
+if ((totalSpentTime + worklogResult.getWorklog().getTimeSpent())/3600 > 16){
+  return "帅哥，你今天的工时已经达到了创纪录的16小时！真的假的？！"
+}
+//---------------------------------------------------------------------------------------------
+
+//检查今天是否已登记工时
+//---------------------------------------------------------------------------------------------
+Long totalSpentTimeCurrentIssue = 0
+
+for (Worklog worklog in currentIssueWorklog){
+    if(worklog.getStartDate().getTime() > parsedDate.getTime()){
+    	totalSpentTimeCurrentIssue += worklog.getTimeSpent()
+    }
+}
+if (totalSpentTimeCurrentIssue == 0 && worklogResult.getWorklog().getTimeSpent() == 3){
+  return "请登记今天的工时"
+}
+//---------------------------------------------------------------------------------------------
+
+//检查输入的时间
+//if (worklogResult.getWorklog().getTimeSpent() < 600) { 
+//  return "确定只花了不到10分钟处理这件事情吗？如果只是想要推迟处理，请选择“暂停处理”然后选择推迟时间。";
+//}
+
+
 //return worklog.getIssue().getStatus().getName()
 //if(worklog.getIssue().getStatus().getStatusCategory().getName() != "In Progress"){
   //return worklog.getIssue().getStatus().getStatusCategory().getName()
   //return "当前事务未处于处理状态。"
 //}
 
-//制定类型的issue不能记录工时
+//指定类型的issue不能记录工时
 //---------------------------------------------------------------------------------------------
 if(worklog.getIssue().getProjectObject().getKey() == "REVIEW"){
   return "请勿在该类型事务下记录工时。"
@@ -105,7 +122,7 @@ def result = issueManager.updateIssue(systemUser, mutableIssue, EventDispatchOpt
 if (result == "")
   return "更新失败，请通知管理员检查WorklogPro Scripts"
 //---------------------------------------------------------------------------------------------
-//if(currentUser.getName() != "xxx"){
+//if(currentUser.getName() != "hehaitao"){
 //  def workDescription = worklog.getComment();
 //  if (workDescription == null || workDescription.length() < 1){
 //    return "请输入工作内容（Work Description）。"
